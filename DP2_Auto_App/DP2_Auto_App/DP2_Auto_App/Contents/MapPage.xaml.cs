7680afc60ee3614ec0883fe0a88c07862841748d
Javier Arias <skeletpiece@hotmail.com>
2017-10-27 12:33:24 -0500

    mapas con estadisticas

--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/MapPage.xaml.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/MapPage.xaml.cs
@@ -16,19 +16,18 @@ namespace DP2_Auto_App.Contents
         public MapPage()
         {
             InitializeComponent();
-            /* Crear el mapa con C#. Por ahora se esta usando el nativo de Xamarin
-             * var map = new Map( 
-                MapSpan.FromCenterAndRadius(
-                    new Position(-12.0689857, -77.078947), 
-                    Distance.FromMiles(0.5)))
-            {
-                IsShowingUser = true,
-                HeightRequest = 100,
-                WidthRequest = 960,
-                VerticalOptions = LayoutOptions.FillAndExpand
-            };
-            */
+            initializeMap();
+        }
 
+        private void map_slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var zoomLevel = e.NewValue; // between 1 and 18
+            var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+            map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+        }
+
+        private void initializeMap()
+        {
             var position = new Position(-12.0689857, -77.078947); // Latitude, Longitude
             var pin = new Pin
             {
@@ -49,13 +48,6 @@ namespace DP2_Auto_App.Contents
             var stack = new StackLayout { Spacing = 0 };
             stack.Children.Add(map);
             Content = stack;
-        }
-
-        private void map_slider_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            var zoomLevel = e.NewValue; // between 1 and 18
-            var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
-            map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
         }
     }
 }
