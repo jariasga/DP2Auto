2446a36fb3b703fa0d651c162fc4616774fe94cc
Pablo M <pmanriquez@pucp.pe>
2017-11-03 04:46:08 -0500

    pantallas estacionamiento, sensores y recordatorio

--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/MainMenu.xaml.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/MainMenu.xaml.cs
@@ -32,8 +32,8 @@ namespace DP2_Auto_App.Contents
             else if (item.Title.Equals("Objetivos y logros")) Detail = new NavigationPage(page);
             else if (item.Title.Equals("Estacionamiento")) Detail = new NavigationPage(new Contents.Parking());
             else if (item.Title.Equals("Navegación y Viajes")) Detail = new NavigationPage(new Contents.MapTabbedPage());
-            else if (item.Title.Equals("Recordatorios")) Detail = new NavigationPage(page);
-            else if (item.Title.Equals("Estado del Auto")) Detail = new NavigationPage(page);
+            else if (item.Title.Equals("Recordatorios")) Detail = new NavigationPage(new Contents.ReminderPage());
+            else if (item.Title.Equals("Estado del Auto")) Detail = new NavigationPage(new Contents.SensorPage());
             else if (item.Title.Equals("Seguridad")) Detail = new NavigationPage(new Contents.SecurityPage());
             else if (item.Title.Equals("Bluetooth")) Detail = new NavigationPage(new Contents.Bluetooth());
             else Detail = new NavigationPage(page);
