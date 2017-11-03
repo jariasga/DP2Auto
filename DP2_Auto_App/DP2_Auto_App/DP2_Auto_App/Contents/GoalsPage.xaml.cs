b42f6f8883f7375fb27a2e4c8e880aa73e99689c
Pablo M <pmanriquez@pucp.pe>
2017-11-03 16:55:31 -0500

    tmr

--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/GoalsPage.xaml.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/GoalsPage.xaml.cs
@@ -16,5 +16,23 @@ namespace DP2_Auto_App.Contents
         {
             InitializeComponent();
         }
+
+        private void buttonClickedAchievedGoals(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Correcto", "Ok");
+            App.Current.MainPage = new Contents.GoalsAchieved();
+        }
+
+        private void buttonClickedActualGoals(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Correcto", "Ok");
+            App.Current.MainPage = new Contents.GoalsActual();
+        }
+
+        private void buttonClickedNewGoals(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Correcto", "Ok");
+            App.Current.MainPage = new Contents.GoalsNew();
+        }
     }
 }
--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/MainMenu.xaml.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/MainMenu.xaml.cs
@@ -29,7 +29,7 @@ namespace DP2_Auto_App.Contents
             // --
 
             if (item.Title.Equals("Usuario")) Detail = new NavigationPage(page);
-            else if (item.Title.Equals("Objetivos y logros")) Detail = new NavigationPage(page);
+            else if (item.Title.Equals("Objetivos y logros")) Detail = new NavigationPage(new Contents.GoalsPage());
             else if (item.Title.Equals("Estacionamiento")) Detail = new NavigationPage(new Contents.Parking());
             else if (item.Title.Equals("Navegación y Viajes")) Detail = new NavigationPage(new Contents.MapTabbedPage());
             else if (item.Title.Equals("Recordatorios")) Detail = new NavigationPage(new Contents.ReminderPage());
