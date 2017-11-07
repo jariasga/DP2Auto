42a0f6dc54327673df7d0712572f3d427e7e8808
Javier Arias <skeletpiece@hotmail.com>
2017-11-07 18:37:15 -0500

    client Web Data & menuChanges

--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Models/RestServices/JSON_Classes.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Models/RestServices/JSON_Classes.cs
@@ -11,4 +11,20 @@ namespace DP2_Auto_App.Models.RestServices
         public string email { get; set; }
         public string password { get; set; }
     }
+
+    public class Client
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int organization_id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string phone { get; set; }
+        public int rating { get; set; }
+        public object deleted_at { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public string token { get; set; }
+    }
 }
