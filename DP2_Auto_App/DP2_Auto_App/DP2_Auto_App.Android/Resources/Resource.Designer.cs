ada7381cfce575d18fe3c30a8c045015f1705253
Pablo M <pmanriquez@pucp.pe>
2017-11-09 19:37:10 -0500

    pichicateando algunas vistas

--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App.Android/Resources/Resource.Designer.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App.Android/Resources/Resource.Designer.cs
@@ -2456,26 +2456,26 @@ namespace DP2_Auto_App.Droid
 			// aapt resource value: 0x7f020055
 			public const int avd_hide_password = 2130837589;
 			
-			// aapt resource value: 0x7f020152
-			public const int avd_hide_password_1 = 2130837842;
-			
 			// aapt resource value: 0x7f020153
-			public const int avd_hide_password_2 = 2130837843;
+			public const int avd_hide_password_1 = 2130837843;
 			
 			// aapt resource value: 0x7f020154
-			public const int avd_hide_password_3 = 2130837844;
+			public const int avd_hide_password_2 = 2130837844;
+			
+			// aapt resource value: 0x7f020155
+			public const int avd_hide_password_3 = 2130837845;
 			
 			// aapt resource value: 0x7f020056
 			public const int avd_show_password = 2130837590;
 			
-			// aapt resource value: 0x7f020155
-			public const int avd_show_password_1 = 2130837845;
-			
 			// aapt resource value: 0x7f020156
-			public const int avd_show_password_2 = 2130837846;
+			public const int avd_show_password_1 = 2130837846;
 			
 			// aapt resource value: 0x7f020157
-			public const int avd_show_password_3 = 2130837847;
+			public const int avd_show_password_2 = 2130837847;
+			
+			// aapt resource value: 0x7f020158
+			public const int avd_show_password_3 = 2130837848;
 			
 			// aapt resource value: 0x7f020057
 			public const int check = 2130837591;
@@ -3194,11 +3194,11 @@ namespace DP2_Auto_App.Droid
 			// aapt resource value: 0x7f020145
 			public const int notification_icon_background = 2130837829;
 			
-			// aapt resource value: 0x7f020150
-			public const int notification_template_icon_bg = 2130837840;
-			
 			// aapt resource value: 0x7f020151
-			public const int notification_template_icon_low_bg = 2130837841;
+			public const int notification_template_icon_bg = 2130837841;
+			
+			// aapt resource value: 0x7f020152
+			public const int notification_template_icon_low_bg = 2130837842;
 			
 			// aapt resource value: 0x7f020146
 			public const int notification_tile_bg = 2130837830;
@@ -3229,6 +3229,9 @@ namespace DP2_Auto_App.Droid
 			
 			// aapt resource value: 0x7f02014f
 			public const int tooltip_frame_light = 2130837839;
+			
+			// aapt resource value: 0x7f020150
+			public const int work = 2130837840;
 			
 			static Drawable()
 			{
