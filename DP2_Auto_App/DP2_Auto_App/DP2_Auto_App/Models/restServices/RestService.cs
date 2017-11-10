1d7e8d6f10d57b622f0e6c0b2585d11437d09577
Javier Arias <skeletpiece@hotmail.com>
2017-11-10 00:39:47 -0500

    more services

--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Models/restServices/RestService.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Models/restServices/RestService.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http.Headers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace DP2_Auto_App.Models.RestServices
 {
@@ -18,6 +20,8 @@ namespace DP2_Auto_App.Models.RestServices
         HttpClient webClient;
         Uri baseAddress, uri;
         public static Client client { get; private set; }
+        private string temporalTokenSave;
+        List<Travel> travels;
 
         public RestService()
         {
@@ -65,7 +69,94 @@ namespace DP2_Auto_App.Models.RestServices
 
                 if (response.IsSuccessStatusCode)
                 {
+                    temporalTokenSave = client.token;
                     client = JsonConvert.DeserializeObject<Client>(rString);
+                    client.token = temporalTokenSave;
+                    return rString;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return null;
+        }
+
+        public async Task<string> updateClientInfo()
+        {
+            uri = new Uri(baseAddress, "clients/" + client.id);
+
+            webClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", client.token);
+
+            var json = JsonConvert.SerializeObject(client);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await webClient.PutAsync(uri, content);
+                var rString = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return rString;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return null;
+        }
+
+        public async Task<string> startTravel(string macAddress)
+        {
+            webClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", client.token);
+            uri = new Uri(baseAddress, "travels");
+
+            clientMAC mac = new clientMAC
+            {
+                vehicle_mac = macAddress
+            };
+            startTravel travel = new startTravel();
+
+            var json = JsonConvert.SerializeObject(mac);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await webClient.PostAsync(uri, content);
+
+                var rString = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    travel = JsonConvert.DeserializeObject<startTravel>(rString);
+                    return rString;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return null;
+        }
+
+        public async Task<string> endTravel(startTravel travel)
+        {
+            webClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", client.token);
+            uri = new Uri(baseAddress, "travels/" + travel.id);
+                        
+            var content = new StringContent("", Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await webClient.PutAsync(uri, content);
+
+                var rString = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    travel = JsonConvert.DeserializeObject<Travel>(rString);
                     return rString;
                 }
             }
