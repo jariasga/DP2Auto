dc7d689d1b381b908ad4408538f0bbf382726e23
Javier Arias <skeletpiece@hotmail.com>
2017-11-17 00:02:36 -0500

    more threads

--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/SensorPage.xaml.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/SensorPage.xaml.cs
@@ -7,31 +7,46 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using DP2_Auto_App.Models.RestServices;
+using System.Threading;
 
 namespace DP2_Auto_App.Contents
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SensorPage : ContentPage
     {
+        
         public SensorPage()
         {
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            label_speed.Text = await webService.rest.getReadingInfo(Readings.SPEED) + " km/h";
-            label_temperature.Text = await webService.rest.getReadingInfo(Readings.TEMPERATURE) + " C";
-            label_weight.Text = await webService.rest.getReadingInfo(Readings.WEIGHT) + " Kg";
-            label_pulse.Text = await webService.rest.getReadingInfo(Readings.PULSE) + " p/m";
-            label_proximity.Text = await webService.rest.getReadingInfo(Readings.PROXIMITY) + " m";
-            label_battery.Text = await webService.rest.getReadingInfo(Readings.BATTERY) + " %";
-            await DisplayAlert("Correcto", "Actualizado", "Ok");
+            Task.Run(async () => updateSensorValues());
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
+            //Create random data
 
+        }
+
+        private Task updateSensorValues()
+        {
+            while (true)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    label_speed.Text = webService.rest.getReadingInfo(Readings.SPEED) + " km/h";
+                    label_temperature.Text = webService.rest.getReadingInfo(Readings.TEMPERATURE) + " C";
+                    label_weight.Text = webService.rest.getReadingInfo(Readings.WEIGHT) + " Kg";
+                    label_pulse.Text = webService.rest.getReadingInfo(Readings.PULSE) + " p/m";
+                    label_proximity.Text = webService.rest.getReadingInfo(Readings.PROXIMITY) + " m";
+                    label_battery.Text = webService.rest.getReadingInfo(Readings.BATTERY) + " %";
+                    DisplayAlert("Correcto", "Actualizado", "Ok");
+                    Task.Delay(2000);
+                });
+            }
         }
     }
 }
