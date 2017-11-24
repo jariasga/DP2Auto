89b2688478aca492efec03938b75b055316c1522
Pablo Manriquez <pmanriquez@pucp.pe>
2017-11-24 18:06:34 -0500

    xd

--- DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/RemindersListViewPage.xaml.cs
+++ DP2_Auto_App/DP2_Auto_App/DP2_Auto_App/Contents/RemindersListViewPage.xaml.cs
@@ -16,17 +16,29 @@ namespace DP2_Auto_App.Contents
     public partial class RemindersListViewPage : ContentPage
     {
         private List<Reminder> reminders { get; set; }
+        public static bool actualLoop;
+        public static bool unico;
         public RemindersListViewPage()
         {
             InitializeComponent();
+            actualLoop = true;
+            unico = false;
             initializeValues();
         }
 
         private async void initializeValues()
         {
-            reminders = await webService.rest.listReminders();
-            MyListView.ItemsSource = reminders;
-            MyListView.IsPullToRefreshEnabled = true;
+            while (actualLoop)
+            {
+                reminders = await webService.rest.listReminders();
+                MyListView.ItemsSource = reminders;
+                MyListView.IsPullToRefreshEnabled = true;
+
+                await Task.Delay(8000);
+                comprobarRecordatorio();
+            }
+
+
         }
 
         async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
@@ -41,32 +53,32 @@ namespace DP2_Auto_App.Contents
             //Deselect Item
             ((ListView)sender).SelectedItem = null;
 
-            alertRemind();
+            //alertRemind();
 
 
 
         }
 
-        private void alertRemind()
+        private async void comprobarRecordatorio()
         {
             string dia, hora, identificador, aux;
             dia = DateTime.Now.ToString("dd/MM/yyyy");
             identificador = DateTime.Now.ToString("tt", CultureInfo.InvariantCulture);
             aux = identificador.ToLower();
             hora = string.Concat(DateTime.Now.ToString("hh:mm "), aux);
-            int contador = reminders.Count();
-            for (int i = 0; i <= (contador - 1); i++)
+            int contador = reminders.Count()-1;
+            for (int i = 0; i <= contador; i++)
             {
                 if (reminders[i].end_date == dia)
                 {
-                    DisplayAlert("Atención!", string.Concat("Hoy: ", reminders[i].description), "OK");
+                    await DisplayAlert("Atención!", string.Concat("Hoy: ", reminders[i].description), "OK");
                 }
             }
         }
 
         private void button_Remember_Clicked(object sender, EventArgs e)
         {
-            alertRemind();
+            //alertRemind();
         }
 
         /*private void MenuItem_Clicked(object sender, EventArgs e)
